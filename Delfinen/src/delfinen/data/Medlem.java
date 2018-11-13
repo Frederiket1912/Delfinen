d403f8b76f5bce2a22ae31063a50a2ba05bb2015
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2018-11-13 11:17:32 +0100

    Merge origin/master
    Conflicts:
    	Delfinen/src/delfinen/Delfinen.java

--- Delfinen/src/delfinen/data/Medlem.java
+++ Delfinen/src/delfinen/data/Medlem.java
@@ -10,4 +10,87 @@ public class Medlem {
     private boolean medlemskabsstatus;
     private String mail;
     private Betaling betaling;
+
+    public Medlem(String name, int cprnr, boolean medlemskabsstatus, String mail, Betaling betaling) {
+        this.name = name;
+        this.cprnr = cprnr;
+        this.medlemskabsstatus = medlemskabsstatus;
+        this.mail = mail;
+        this.betaling = betaling;
+    }
+    
+    public void visMedlem(Medlem medlem){
+        jtextfieldName.setText(medlem.getName());
+        jtextfieldCprnr.setText(medlem.getCprnr());
+        jtextfieldMail.setText(medlem.getMail());
+        if (medlemskabsstatus){
+            jradiobuttonAktiv.doClick();
+        }
+        else {
+            jradibuttonPassiv.doClick();
+        }      
+    }
+    
+    public void gemÆndringer(Medlem medlem){
+        String name = jtextfieldName.getText();
+        int cprnr = Integer.parseInt(jtextfieldCprnr.getText());
+        String mail = jtextfieldMail.getText();
+        if (jradiobuttonAktiv.isSelected()){
+            medlemskabsstatus = true;
+        }
+        else{
+            medlemskabsstatus = false;
+        }
+        medlem.setName(name);
+        medlem.setCprnr(cprnr);
+        medlem.setMail(mail);
+        medlem.setMedlemskabsstatus(medlemskabsstatus);
+    }
+    
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCprnr() {
+        return cprnr;
+    }
+
+    public void setCprnr(int cprnr) {
+        this.cprnr = cprnr;
+    }
+
+    public boolean isMedlemskabsstatus() {
+        return medlemskabsstatus;
+    }
+
+    public void setMedlemskabsstatus(boolean medlemskabsstatus) {
+        this.medlemskabsstatus = medlemskabsstatus;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Betaling getBetaling() {
+        return betaling;
+    }
+
+    public void setBetaling(Betaling betaling) {
+        this.betaling = betaling;
+    }
+    
+    
+    
+
 }
