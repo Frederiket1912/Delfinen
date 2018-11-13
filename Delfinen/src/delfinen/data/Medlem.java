cf7e63c6ba283e7f1e88bc3105bf18511f24e362
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2018-11-13 10:32:56 +0100

    Merge origin/master
    Conflicts:
    	Delfinen/src/delfinen/Delfinen.java

---
+++ Delfinen/src/delfinen/data/Medlem.java
@@ -0,0 +1,13 @@
+
+package delfinen.data;
+
+import delfinen.logic.Betaling;
+
+
+public class Medlem {
+    private String name;
+    private int cprnr;
+    private boolean medlemskabsstatus;
+    private String mail;
+    private Betaling betaling;
+}
