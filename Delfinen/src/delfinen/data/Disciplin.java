53bacd3bb55252c2d6722e8aa25a2be0e77957d3
Ahmad Talha Noory <talha.nuri@gmail.com>
2018-11-14 11:02:17 +0100

    Merge origin/master
    Conflicts:
    	Delfinen/src/delfinen/Delfinen.java

---
+++ Delfinen/src/delfinen/data/Disciplin.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfinen.data;
+
+/**
+ *
+ * @author Talha Noory
+ */
+public class Disciplin {
+    
+    public enum resultat {
+        
+        CRAWL, RYGCRAWL, BUTTERFLY, BRYSTSVØMNING
+    }
+    
+}
