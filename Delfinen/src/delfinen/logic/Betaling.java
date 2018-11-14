c3d2defc01a359aa59530ffdb22802d39aa92223
Sebastian Bernth Jepsen <sbj@proxus.dk>
2018-11-14 10:11:51 +0100

    vitester

--- Delfinen/src/delfinen/logic/Betaling.java
+++ Delfinen/src/delfinen/logic/Betaling.java
@@ -1,10 +1,22 @@
 
 package delfinen.logic;
 
+import delfinen.data.Medlem;
+
 
 public class Betaling {
-
+    
+    private int year;
+    
     public Betaling() {
+        
+    }
+    public int udregnBetaling(Medlem medlem, int year){
+        int age;
+        age = medlem.getFødselsdato()-year;
+        if(age < 18) return 1000;
+        if(age >= 18) return 1600;
+        if(age >= 60) return 
     }
     
 }
