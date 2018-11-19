1a799f5a6f079ea8aace0a0a42ec573ac2339162
Frederik juel <frederikhurup@gmail.com>
2018-11-19 12:37:24 +0100

    Merge origin/master
    Conflicts:
    	Delfinen/nbproject/private/private.properties

--- Delfinen/src/delfinen/presentation/OpretMedlem.java
+++ Delfinen/src/delfinen/presentation/OpretMedlem.java
@@ -6,7 +6,6 @@
 package delfinen.presentation;
 
 import delfinen.data.DataAccessorFile;
-import delfinen.logic.BetalingsCalculator;
 import delfinen.logic.Controller;
 import java.util.ArrayList;
 import java.util.Enumeration;
